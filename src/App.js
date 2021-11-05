1e7dfee940f1f97c2cc052d999259f4d3c098255
jaredhirsch-google <66283177+jaredhirsch-google@users.noreply.github.com>
2021-11-05 00:51:13 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+function VeryBeautifulNumber(num) {
+  return <span> {num} </span>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>How many elements can you add?</h1>1 2 3 ...
+    </div>
+  );
+  // Hint: If you add too many elements you might break your program
+}
